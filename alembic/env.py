d341826a1a24a00a33ff599648e2706607210b12
Robel Kebede <robel.kebede@bison.howard.edu>
2025-12-25 12:06:30 +0300

    Patch for database url sync bug

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from app.models.base import Base
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL_SYNC)
+config.set_main_option("sqlalchemy.url", settings.database_url_sync)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
